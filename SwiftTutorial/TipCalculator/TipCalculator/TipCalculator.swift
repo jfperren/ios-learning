97cb56de7a226d76b26c911fda049bf2f4795ee9
jfperren <paulineeeckhout@tsf-444-wpa-0-186.epfl.ch>
2015-05-24 18:57:22 +0200

    Finished SwiftTutorial Part 2

---
+++ SwiftTutorial/TipCalculator/TipCalculator/TipCalculator.swift
@@ -0,0 +1,9 @@
+//
+//  TipCalculator.swift
+//  TipCalculator
+//
+//  Created by Pauline Eeckhout on 24/05/15.
+//  Copyright (c) 2015 Julien Perrenoud. All rights reserved.
+//
+
+import Foundation
